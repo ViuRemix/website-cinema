e533ed04c4e029c4c8e20002baa4646298bffe91
ViuRemix <worksor.78@gmail.com>
2024-11-14 21:03:23 +0700

    Add to header.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-// src/App.js
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-import PrivacyPolicy from "./pages/PrivacyPolicy"; // Import the Privacy Policy page
+import Header from "./components/Header";
 import Footer from "./components/Footer";
+import PrivacyPolicy from "./pages/PrivacyPolicy"; // Import trang Chính sách quyền riêng tư
 
 function App() {
   return (
     <Router>
-      <div>
+      <div className="App">
+        <Header />
         <Routes>
+          {/* Home here */}
+          {/* <Route path="/" element={<Home />} /> Trang Home */}
           <Route path="/privacy-policy" element={<PrivacyPolicy />} />
         </Routes>
-        
+
         {/* Footer nằm ngoài Routes để luôn hiển thị */}
         <Footer />
       </div>
     </Router>
-
   );
 }
 
 export default App;
-
