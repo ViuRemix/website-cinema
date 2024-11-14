d3937366953934e2b03c68222a1f3732ac0d68ae
ViuRemix <worksor.78@gmail.com>
2024-11-14 14:33:06 +0700

    add to PrivacyPolicy

--- src/pages/PrivacyPolicy.js
+++ src/pages/PrivacyPolicy.js
@@ -1,4 +1,4 @@
-// src/pages/PrivacyPolicy.js
+// src/PrivacyPolicy.js
 import React from "react";
 
 const PrivacyPolicy = () => {
@@ -9,6 +9,19 @@ const PrivacyPolicy = () => {
       <p>
         Thông tin chi tiết về cách chúng tôi thu thập và sử dụng dữ liệu người
         dùng...
+      </p>
+
+      {/* Thêm phần Chính sách xóa dữ liệu */}
+      <p>
+        <strong>Chính sách xóa dữ liệu:</strong>
+      </p>
+      <p>
+        Người dùng có quyền yêu cầu xóa dữ liệu cá nhân của họ khỏi hệ thống của
+        chúng tôi. Để yêu cầu xóa dữ liệu, vui lòng liên hệ với chúng tôi qua
+        email:
+        <a href="mailto:viu106018@donga.edu.vn">viu106018@donga.edu.vn</a>
+        Chúng tôi cam kết xử lý yêu cầu xóa dữ liệu trong thời gian 30 ngày làm
+        việc.
       </p>
     </div>
   );
