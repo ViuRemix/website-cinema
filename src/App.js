e073487702829bf799c303305f0e933648a6849f
ViuRemix <worksor.78@gmail.com>
2024-11-15 08:54:38 +0700

    add to login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import LoginForm from "./components/LoginForm"; // Trang đăng nhập
+// Import các trang khác nếu cần
 // import Home from "./pages/Home";
 // import MovieDetail from "./pages/MovieDetail"; // Trang chi tiết phim
-import LoginForm from "./components/LoginForm"; // Trang đăng nhập
 // import SignupForm from "./components/SignupForm"; // Trang đăng ký
 
 function App() {
+  const [user, setUser] = useState(null); // Quản lý trạng thái người dùng
+
   return (
     <Router>
       <div className="App">
-        <Header />
+        <Header user={user} setUser={setUser} />{" "}
+        {/* Truyền user và setUser vào Header */}
         <Routes>
-          {/* <Route path="/" element={<Home />} />
-          cho chọn phim
-          <Route path="/genres/:genre" element={<Home />} />
-          <Route path="/movie/:movieId" element={<MovieDetail />} /> */}
-          <Route path="/login" element={<LoginForm />} />{" "}
+          {/* Các route khác */}
+          <Route path="/login" element={<LoginForm setUser={setUser} />} />{" "}
+          {/* Truyền setUser vào LoginForm */}
+          {/* <Route path="/" element={<Home />} /> */}
+          {/* <Route path="/movie/:movieId" element={<MovieDetail />} /> */}
+          {/* <Route path="/genres/:genre" element={<Home />} /> */}
         </Routes>
         <Footer />
       </div>
